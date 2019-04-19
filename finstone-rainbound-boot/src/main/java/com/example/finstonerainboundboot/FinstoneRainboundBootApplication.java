d4cac41ea494025e002d333154d814133621bea9
skyzhangyf <sky_zhangyf@163.com>
2019-04-19 14:46:01 +0800

    Initial commit

---
+++ finstone-rainbound-boot/src/main/java/com/example/finstonerainboundboot/FinstoneRainboundBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.finstonerainboundboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinstoneRainboundBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( FinstoneRainboundBootApplication.class, args );
+    }
+
+}
---
+++ finstone-rainbound-boot/src/main/java/com/example/finstonerainboundboot/TestController.java
@@ -0,0 +1,18 @@
+package com.example.finstonerainboundboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangyf
+ * @description
+ * @date 2019/4/19/019
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/hello")
+    public String helloWord() {
+        return "Hello word!";
+    }
+}
